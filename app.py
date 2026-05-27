17f17e931e9180c46ba55cedc2b41192daba8be8
Theharddests <jeanmomoabdel@gmail.com>
2026-05-27 09:50:59 +0100

    fix port render

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Mon mini VPS fonctionne"
+
 port = int(os.environ.get("PORT", 10000))
 
-app.run(host="0.0.0.0", port=8080)
+app.run(host="0.0.0.0", port=port)
